c2afb20a8598a4f9c77bc3e575fb7380e3b5e394
AdamAFD <ahmed.adam.2078@gmail.com>
2022-12-06 17:15:42 +0200

    week 4

---
+++ week 4/task 1/task.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+echo "enter your appname"
+
+APP_NAME=$1
+
+echo "enter your app-url"
+REPO_URL=$2
+
+echo "enter your linux user"
+
+usernamee=$3
+
+HTTPS_REPO_URL="https://$(echo  $REPO_URL | sed 's~http[s]*://~~g')"
+
+echo $HTTPS_REPO_URL
+
+docker system prune -f
+ 
+rm -rf $APP_NAME
+
+mkdir $APP_NAME
+
+cd  ./$APP_NAME
+
+git clone $HTTPS_REPO_URL
+
+git checkout main
+
+export uid=1000
+
+export user=$usernamee
+
+docker build . -t $APP_NAME:$(date +"%d-%m-%y")
+docker images
+docker login registry.gitlab.com -u squadx -p Q_rKzt2Qmxazm82Ws3Em
+docker push $APP_NAME:$(date +"%d-%m-%y")
+rm -rf $APP_NAME
+docker system prune -f 
